fa3d5df889ba9560712a8533f3987de814151736
thalison italo <thalison_italo1@hotmail.com>
2023-02-06 09:44:58 -0300

    exercicio js

---
+++ exercicio.js
@@ -0,0 +1,15 @@
+function aa() {
+
+    document.querySelector("div").innerText = "clicou!"
+
+}
+
+function bb() {
+
+    document.querySelector("div").innerText = "passou o mouse!"
+}
+
+function cc() {
+
+    document.querySelector("div").innerText = "tirou o mouse!"
+}
---
+++ script.js
@@ -0,0 +1,20 @@
+function clickMe(){
+    console.log("fui clicado")
+}
+
+function focusMe() {
+    console.log("fui focado")
+}
+
+function blurMe(){
+    console.log("fui desfocado")
+} 
+
+
+function changeMe() {
+    console.log("fui alterado")
+}
+
+function overMe(){
+    console.log("mouse aqui")
+}
