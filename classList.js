50eac0ff7836a8e158c21f62981fbb9f7d9222af
thalison italo <thalison_italo1@hotmail.com>
2023-02-07 11:25:40 -0300

    elementos

---
+++ classList.js
@@ -0,0 +1,10 @@
+
+
+function aa (){
+    document.querySelector("#minhaDiv").classList.add("classe1","classe2","classe3")
+}
+
+
+function bb (){
+    document.querySelector("#minhaDiv").classList.remove("classe1","classe2","classe3")
+}
---
+++ elementos.js
@@ -0,0 +1,7 @@
+function aa() {
+    const paragrafo = document.createElement('p')
+
+    paragrafo.innerHTML = "isso é um paragrafo"
+
+    document.body.appendChild(paragrafo)
+}
